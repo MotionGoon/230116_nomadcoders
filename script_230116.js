b3b63ef3367fda3b4a6d3c9e2b239259e76ec3a1
Goon <escjk@hanmail.net>
2023-01-16 19:45:27 +0900

    230116_nomadcoders

---
+++ script_230116.js
@@ -0,0 +1,15 @@
+// Arrays(배열);
+// const arrays = ["apple", "banana", "orenge"];
+// console.log(arrays[0]);
+
+// Object(오브젝트);
+// const player = {
+//   name: "Goon",
+//   points: 10,
+//   Handsome: true,
+//   fat: "little bit",
+// };
+
+// console.log(player.name);
+// player.points = player.points + 15;
+// console.log(player.points);
